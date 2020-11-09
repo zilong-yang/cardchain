19cd725390a7b2ecad2923010d73af5be6ab9d44
Ben Meagher <55548888+meagherb@users.noreply.github.com>
2020-11-09 02:00:30 -0500

    Intial PlayerToken contract and playerToken folder with migration files. Note: migration has a bug

---
+++ playerToken/migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+const PlayerToken = atrifacts.require("./PlayerToken.sol");
+
+module.exports = function(deployer, network, accounts) {
+    //could be an issue with accounts[0]
+    deployer.deploy(PlayerToken, accounts[0])
+};
